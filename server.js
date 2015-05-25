9305af0b33440ded658de7bb3184e4338411bc90
Mads Kølbæk <mk@uvdata.dk>
2015-05-25 14:41:58 +0200

    Alt

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ var SampleApp = function() {
 
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
+            self.app.use(r, self.routes[r]);
         }
     };
 
